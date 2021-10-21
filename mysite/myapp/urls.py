bfbeb1a2977413b1d1641820049afa130644f3da
OldKorshunov <dmkorshunov2013@gmail.com>
2021-10-21 22:38:51 +0300

    first try

---
+++ mysite/myapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from .views import (ProductListView, Login, Register, Logout, ProductUpdateView, ProductCreateView)
+
+urlpatterns = [
+    path('', ProductListView.as_view(), name='index'),
+    path('login/', Login.as_view(), name='login'),
+    path('register/', Register.as_view(), name='register'),
+    path('logout/', Logout.as_view(), name='logout'),
+    path('product/create/', ProductCreateView.as_view(), name='product-create'),
+    path('product/update/<int:pk>/', ProductUpdateView.as_view(), name='product-update'),
+    # path('product/purchase/<int:pk>/', Purchase.as_view(), name='purchase'),
+    # path('product/purchases/<int:pk>/', Purchase.as_view(), name='purchase'),
+]
---
+++ mysite/myapp/views.py
@@ -0,0 +1,72 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import ListView, CreateView, UpdateView
+
+from .models import *
+from .forms import ProductCreateForm, SignUpForm
+# Create your views here.
+
+
+class ProductListView(LoginRequiredMixin, ListView):
+    model = Product
+    template_name = 'index.html'
+    login_url = 'login/'
+    extra_context = {'create_form': ProductCreateForm()}
+    paginate_by = 5
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['num_visits'] = self.request.session['num_visits']
+    #     return context
+    #
+    # def get(self, request, *args, **kwargs):
+    #     num_visits = request.session.get('num_visits', 0)
+    #     request.session['num_visits'] = num_visits + 1
+    #     if num_visits >= 4:
+    #         request.session['num_visits'] = 1
+    #     return super().get(request, *args, **kwargs)
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+class Login(LoginView):
+    template_name = 'login.html'
+
+
+class Register(CreateView):
+    form_class = SignUpForm
+    template_name = 'register.html'
+    success_url = '/'
+
+
+class Logout(LoginRequiredMixin, LogoutView):
+    next_page = '/'
+    login_url = 'login/'
+
+
+class ProductCreateView(LoginRequiredMixin, CreateView):
+    login_url = 'login/'
+    http_method_names = ['post']
+    form_class = ProductCreateForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        obj = form.save(commit=False)
+        obj.save()
+        return super().form_valid(form=form)
+
+
+class ProductUpdateView(LoginRequiredMixin, UpdateView):
+    model = Product
+    form_class = ProductCreateForm
+    template_name = 'index.html'
+    success_url = '/'
+
+#
+# class Purchase(LoginRequiredMixin, CreateView):
+#     model = Purchase
